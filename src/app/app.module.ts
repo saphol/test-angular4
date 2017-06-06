b40ae13064fccc79e505fdaf15ca1a72a83a27b3
d9bdk <katika.sum@gmail.com>
2017-06-06 15:11:12 +0700

    Add content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+import { EventComponent } from './event/event.component';
+import { AdsComponent } from './ads/ads.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    EventComponent,
+    AdsComponent
   ],
   imports: [
     BrowserModule,
