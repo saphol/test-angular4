998df7c4424d2872884b402d389a0d7d9a74cc37
Saphol's Seoul <tosaphol.h@entronica.co.th>
2017-06-06 15:21:50 +0700

    merge F'num

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { ContentComponent } from './content/content.component';
 import { EventComponent } from './event/event.component';
 import { AdsComponent } from './ads/ads.component';
-=======
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
-import { ContentComponent } from './content/content.component';
->>>>>>> c567b9d54b7c4214b35a78f47433bd64afdc70a6
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     ContentComponent,
     EventComponent,
-    AdsComponent
-=======
+    AdsComponent,
     HeaderComponent,
     MenuComponent,
-    ContentComponent
->>>>>>> c567b9d54b7c4214b35a78f47433bd64afdc70a6
+
   ],
   imports: [
     BrowserModule,
